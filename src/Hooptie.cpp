72aa7e77ad0fe427e1f26574ac8ab67051c0b043
Tom Norris <tom.norris.iv@gmail.com>
2018-04-08 00:30:29 -0400

    adds direction button, minor refactor

--- src/Hooptie.cpp
+++ src/Hooptie.cpp
@@ -4,15 +4,14 @@
 
 #define ROWS 8
 
-
 struct Hooptie : Module {
   enum ParamIds {
     ENUMS(GATE_CHANCE, ROWS),
-    DEBUG_KNOB,
+    MODE,
     NUM_PARAMS
   };
   enum InputIds {
-    BANG_INPUT,
+    BANG_INPUT, // I came from Max/MSP (why you'll see 'bang' when I mean 'gate'/'trigger')
     RST_INPUT,
     NUM_INPUTS
   };
@@ -26,15 +25,22 @@ struct Hooptie : Module {
     NUM_LIGHTS
   };
 
+  enum sequenceDirection { forward, back, ping, random, shotgun, single };
+  sequenceDirection direction = back;
+
   int bangCounter = 0;
 
   SchmittTrigger bangTrigger;
   SchmittTrigger rstTrigger;
+  SchmittTrigger modeTrigger;
 
   bool lastHigh = false;
 
   Hooptie() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
   void step() override;
+
+  void nextSequenceStep();
+  void notify();
 
   float diceRoll = 0.f;
 
@@ -44,25 +50,7 @@ struct Hooptie : Module {
   // - onReset, onRandomize, onCreate, onDelete: implements special behavior when user clicks these from the context menu
 };
 
-
-void Hooptie::step() {
-  bangTrigger.process(inputs[BANG_INPUT].value / 10.f );
-
-  rstTrigger.process(inputs[RST_INPUT].value / 10.f);
-  if(rstTrigger.isHigh()) {
-    bangCounter = 0;
-  }
-
-
-  bool thisHigh = bangTrigger.isHigh();
-
-  if(thisHigh && !lastHigh) {
-    bangCounter += 1;
-    bangCounter = bangCounter % ROWS;
-
-    diceRoll = randomUniform() * 10.f;
-  }
-
+void Hooptie::notify() {
   // maybe send a message out
   // if the roll < chance_knob[bangCounter]: send BANG_INPUT through
   if(diceRoll < params[GATE_CHANCE+bangCounter].value) {
@@ -77,12 +65,73 @@ void Hooptie::step() {
     // light up the bangCounter LED
     lights[GATE_LIGHTS + i].setBrightness((i == bangCounter) ? (1.f) : (0.f));
   }
+}
+
+void Hooptie::nextSequenceStep() {
+  bangTrigger.process(inputs[BANG_INPUT].value / 10.f );
+
+  rstTrigger.process(inputs[RST_INPUT].value / 10.f);
+  if(rstTrigger.isHigh()) {
+    bangCounter = 0;
+  }
+
+  bool thisHigh = bangTrigger.isHigh();
+
+  if(thisHigh && !lastHigh) {
+    switch(direction) {
+      case forward:
+        bangCounter += 1;
+        bangCounter = bangCounter % ROWS;
+        break;
+      case back:
+        bangCounter -= 1;
+        if(bangCounter < 0) { bangCounter = ROWS-1; }
+        break;
+      case ping:
+        break;
+      case random:
+        break;
+      case shotgun:
+        break;
+      case single:
+        if(bangCounter < ROWS) { bangCounter += 1; }
+        break;
+    }
+
+    diceRoll = randomUniform() * 10.f;
+  }
 
   lastHigh = thisHigh;
 }
 
-
-
+void Hooptie::step() {
+  if(modeTrigger.process(params[MODE].value)) {
+    switch(direction) {
+      case forward:
+        direction = back;
+        break;
+      case back:
+        direction = ping;
+        break;
+      case ping:
+        //direction = random;
+        // skip a bit, brother
+        direction = single;
+        break;
+      case random:
+        direction = shotgun;
+        break;
+      case shotgun:
+        direction = single;
+        break;
+      case single:
+        direction = forward;
+        break;
+      }
+  }
+  nextSequenceStep();
+  notify();
+}
 
 struct HooptieWidget : ModuleWidget {
   HooptieWidget(Hooptie *module) : ModuleWidget(module) {
@@ -90,6 +139,9 @@ struct HooptieWidget : ModuleWidget {
     addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 
     addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+    addParam(ParamWidget::create<LEDButton>(Vec(50, 5), module, Hooptie::MODE, 0.0f, 1.0f, 0.0f));
+
 
     for (int i = 0; i < ROWS; i++) {
       // add the chance knob
@@ -110,8 +162,6 @@ struct HooptieWidget : ModuleWidget {
 
     // reset in
     addInput(Port::create<PJ301MPort>(Vec(65, 30+(ROWS*35)), Port::INPUT, module, Hooptie::RST_INPUT));
-
-
   }
 };
 
